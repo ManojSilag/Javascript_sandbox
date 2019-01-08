dfdb2798034944f1bc41bee25b9eba090ca63687
Manoj <silagmanoj@yahoo.in>
2019-01-08 14:15:47 +0530

    Update

--- section7/Project1(github_finder)/app.js
+++ section7/Project1(github_finder)/app.js
@@ -1,1 +1,34 @@
-alert('from app.js')
+//Init Githhub
+const github = new Github;
+
+//Init UI
+const ui = new UI;
+
+//search input
+const searchUser = document.getElementById('searchUser');
+
+//Search input event listner
+searchUser.addEventListener('keyup', (e) =>{
+    //Get input text
+    const userText =e.target.value;
+    if(userText !== ''){
+        console.log(userText);
+
+        //Make http call
+        github.getUser(userText)
+        .then(data =>{
+            //console.log(data.profile);
+            
+            if(data.profile.message === 'Not Found'){
+            //Show alert
+
+            } else{
+            //Show profile
+            ui.showProfile(data.profile);
+            }
+        })
+    } else {
+        //Clear profile
+
+    }
+})
