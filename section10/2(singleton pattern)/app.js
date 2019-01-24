0143fabfb7f7960a8818ac66b682d640397677cc
Manoj <silagmanoj@yahoo.in>
2019-01-24 13:13:39 +0530

    Adding files

---
+++ section10/2(singleton pattern)/app.js
@@ -0,0 +1,24 @@
+//24-1-2019
+
+const singleton = (function(){
+    let instance;
+
+    function createInstance(){
+        const object = new Object({name: 'Manoj'});
+        return object;
+    }
+    return{
+        getInstance: function(){
+            if(!instance){
+                instance = createInstance();
+            }
+            return instance;
+        }
+    }
+})();
+
+const instanceA = singleton.getInstance();
+const instanceB = singleton.getInstance();
+
+console.log(instanceA === instanceB);
+//console.log(instanceA);
