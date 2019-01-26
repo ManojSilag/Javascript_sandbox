2720e1efcb760083511285e9876bfd5cdf76c609
Manoj <silagmanoj@yahoo.in>
2019-01-26 12:15:20 +0530

    Updating

--- section10/4(observe Pattern)/app.js
+++ section10/4(observe Pattern)/app.js
@@ -1,61 +1,56 @@
-
-function EventObserver(){
+function EventObserver() {
     this.observers = [];
-};
-
-EventObserver.prototype = {
-    subscribe: function(fn){
-    this.observers.push(fn);
-    console.log(`you are now subscribed to ${fn.name}`);
+  }
+  
+  EventObserver.prototype = {
+    subscribe: function(fn) {
+      this.observers.push(fn);
+      console.log(`You are now subscribed to ${fn.name}`);
     },
-    unsbscribe: function(fn){
-        this.observers = this.observers.filter(function(item){
-            if(item !== fn){
-                return item;
-            }
-        });
-        console.log(`you are now  unsubscribed from ${fn.name}`);
-        },
-        fire: function(){
-            this.observers.forEach(function(item){
-                item.call();
-            });
+    unsubscribe: function(fn) {
+      // Filter out from the list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filter returns a new list and reassigns the list of observers.
+      this.observers = this.observers.filter(function(item){
+        if(item !== fn) {
+          return item;
         }
-}
-
-const click = new  EventObserver();
-
-//Event listner
-document.querySelector('.sub-ms').addEventListener('click', function(){
-click.subscribe(getCurMillisecond);
-});
-
-
-document.querySelector('.unsub-ms').addEventListener('click', function(){
-click.unsbscribe(getCurMillisecond);
-});
-//-------------//
-document.querySelector('.sub-s').addEventListener('click', function(){
-click.subscribe(getCursecond);
-});
-
-
-document.querySelector('.unsub-s').addEventListener('click', function(){
-click.unsbscribe(getCursecond);
-});
-
-
-document.querySelector('.fire').addEventListener('click', function(){
-click.fire();
-});
-
-
-//click Handler
-const getCurMillisecond = function (){
-    console.log(`current Millisecond: ${new Date().getMilliseconds()}`);
-}
-
-
-const getCursecond = function (){
-    console.log(`current seconds: ${new Date().getSeconds()}`);
-}
+      });
+      console.log(`You are now unsubscribed from ${fn.name}`);
+    },
+    fire: function() {
+      this.observers.forEach(function(item) {
+        item.call();
+      });
+    }
+  }
+  
+  const click = new EventObserver();
+  
+  // Event Listeners
+  document.querySelector('.sub-ms').addEventListener('click', function() {
+    click.subscribe(getCurMilliseconds);
+  });
+  
+  document.querySelector('.unsub-ms').addEventListener('click', function() {
+    click.unsubscribe(getCurMilliseconds);
+  });
+  
+  document.querySelector('.sub-s').addEventListener('click', function() {
+    click.subscribe(getCurSeconds);
+  });
+  
+  document.querySelector('.unsub-s').addEventListener('click', function() {
+    click.unsubscribe(getCurSeconds);
+  });
+  
+  document.querySelector('.fire').addEventListener('click', function() {
+    click.fire();
+  });
+  
+  // Click Handler
+  const getCurMilliseconds = function() {
+    console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+  }
+  
+  const getCurSeconds = function() {
+    console.log(`Current Seconds: ${new Date().getSeconds()}`);
+  }
